4a598b2632d5647108ba989b021825023d1fd51f
OlgaStude <112942851+OlgaStude@users.noreply.github.com>
2023-09-11 16:51:50 +0400

    Add files via upload

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,27 +1,35 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <title>{{env('APP_NAME')}}</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Document</title>
 </head>
+
 <body>
+
     @if (Auth::check())
     <script>
-        window.Laravel = {!!json_encode([
-            'isLoggedin' => true,
-            'user' => Auth::user()
-        ])!!}
+        let globalIsLogged = true;
+    </script>
+    <script>
+        let user = Auth::user()
     </script>
     @else
-        <script>
-            window.Laravel = {!!json_encode([
-                'isLoggedin' => false
-            ])!!}
-        </script>
+    <script>
+        let globalIsLogged = false
+    </script>
     @endif
-    <div id="app"></div>
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+    <div id="app">
+
+    </div>
+
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </body>
+
 </html>
